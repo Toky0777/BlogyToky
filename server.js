3aba9c1e60a16670c7c73c0a77602fa45ccbe28b
TokyHF <74176896+Toky0777@users.noreply.github.com>
2021-08-04 17:55:35 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+
+/*app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+*/
+
+app.get('/', (req, res) => {
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test description'
+    },
+    {
+        title: 'Test Article 2',
+        createdAt: new Date(),
+        description: 'Test description 2'
+    }]
+    res.render('articles/index', { articles: articles })
+})
+
+
+app.use('/articles', articleRouter)
+app.listen(5000)
